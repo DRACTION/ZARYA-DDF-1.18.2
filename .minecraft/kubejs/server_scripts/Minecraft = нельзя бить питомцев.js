8a42107e0f931650febcab8375dbc386d4645ff1
DRACTION <ragdoll98@yandex.ru>
2023-08-08 21:21:48 +1000

    Новый скрипт = не бейте питомцев Он пока сырой, но своё дело делает. Блокирует урон, если: - Игрок бьёт питомца - Питомец бьёт игрока - Питомец бьёт питомца

---
+++ .minecraft/kubejs/server_scripts/Minecraft = нельзя бить питомцев.js
@@ -0,0 +1,37 @@
+
+onEvent('entity.hurt', e => {
+
+    if(e.entity == null || e.source.getActual() == null) return
+    if(e.source.getActual().isPlayer() && e.entity.isPlayer()) return
+
+    let targetHasOwner = e.entity.fullNBT.contains('Owner')
+    let attackerHasOwner   = e.source.getActual().fullNBT.contains('Owner')
+
+    // e.server.tell(`e.entity.fullNBT.contains('Owner') = ${e.entity.fullNBT.contains('Owner')}`)
+    // e.server.tell(`e.source.getActual().isPlayer() = ${e.source.getActual().isPlayer()}`)
+    // Если игрок пытается бить питомца
+    if (targetHasOwner && e.source.getActual().isPlayer()) {
+        // e.server.tell(`игрок пытается бить питомца`)
+        e.cancel()
+        return
+    }
+    // e.server.tell(`e.source.getActual().fullNBT.contains('Owner') = ${e.source.getActual().fullNBT.contains('Owner')}`)
+    // e.server.tell(`e.entity.isPlayer() = ${e.entity.isPlayer()}`)
+    // Если питомец пытается бить игрока
+    if (attackerHasOwner && e.entity.isPlayer()) {
+        // e.server.tell(`питомец пытается бить игрока`)
+        // e.server.tell(`e.source.getActual().getAttackingEntity() = ${e.source.getActual()}`)
+        e.source.getActual().setRevengeTarget(null)
+        // e.server.tell(`e.source.getActual().getAttackingEntity() = ${e.source.getActual().id}`)
+        e.cancel()
+        return
+    }
+    // e.server.tell(`targetHasOwner = ${targetHasOwner}`)
+    // e.server.tell(`attackerHasOwner = ${attackerHasOwner}`)
+    // Если питомец пытается бить питомца
+    if (attackerHasOwner && targetHasOwner) {
+        // e.server.tell(`питомец пытается бить питомца`)
+        e.cancel()
+        return
+    }
+})
