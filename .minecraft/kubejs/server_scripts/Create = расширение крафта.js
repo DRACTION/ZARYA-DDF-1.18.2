f07663c6f8c9b25acca01067bbc96968a6317e7d
DRACTION <ragdoll98@yandex.ru>
2023-09-16 12:55:21 +1000

    Create = новые постройки, палки и кора дерева на пиле: - Новые схемы построек - Получение палок на пиле - добавил кору дерева при обработке пилой

--- .minecraft/kubejs/server_scripts/Create = расширение крафта.js
+++ .minecraft/kubejs/server_scripts/Create = расширение крафта.js
@@ -44,4 +44,66 @@ onEvent('recipes', e => {
     'create:crushed_raw_zinc'
   ).id('zarya:splashing/crushed_raw_zinc')
 
+  ///////////////// Палки из досок /////////////////
+  e.recipes.createCutting(
+    [
+      '4x minecraft:stick',
+      Item.of('minecraft:stick', 5).withChance(0.10)
+    ],
+    '#minecraft:planks'
+  ).id('zarya:cutting/planks_to_sticks')
+
+  e.recipes.createCutting(
+    [
+      '6x minecraft:stick',
+      Item.of('minecraft:stick', 5).withChance(0.10)
+    ],
+    '#zarya:hollow_logs'
+  ).id('zarya:cutting/hollow_logs_to_sticks')
+
+  ///////////////// Кора дерева из бревен /////////////////
+  woods.forEach(name => {
+    e.remove({id:`create:cutting/${name}_log`})
+    e.recipes.createCutting(
+      [
+        `minecraft:stripped_${name}_log`,
+        'farmersdelight:tree_bark',
+        Item.of('farmersdelight:tree_bark', 1).withChance(0.50)
+      ],
+      `minecraft:${name}_log`
+    ).id(`zarya:cutting/${name}_log`)
+
+    e.remove({id:`create:cutting/${name}_wood`})
+    e.recipes.createCutting(
+      [
+        `minecraft:stripped_${name}_wood`,
+        'farmersdelight:tree_bark',
+        Item.of('farmersdelight:tree_bark', 1).withChance(0.50)
+      ],
+      `minecraft:${name}_wood`
+    ).id(`zarya:cutting/${name}_wood`)
+  })
+
+  endWoodNames.forEach(name => {
+    e.remove({id:`create:cutting/${name}_stem`})
+    e.recipes.createCutting(
+      [
+        `minecraft:stripped_${name}_stem`,
+        'farmersdelight:tree_bark',
+        Item.of('farmersdelight:tree_bark', 1).withChance(0.50)
+      ],
+      `minecraft:${name}_stem`
+    ).id(`zarya:cutting/${name}_stem`)
+
+    e.remove({id:`create:cutting/${name}_hyphae`})
+    e.recipes.createCutting(
+      [
+        `minecraft:stripped_${name}_hyphae`,
+        'farmersdelight:tree_bark',
+        Item.of('farmersdelight:tree_bark', 1).withChance(0.50)
+      ],
+      `minecraft:${name}_hyphae`
+    ).id(`zarya:cutting/${name}_hyphae`)
+  })
+
 })
