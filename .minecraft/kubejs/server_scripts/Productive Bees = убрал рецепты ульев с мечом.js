ebb309db1dd9dfc8923404dc75e162779bb2b01a
DRACTION <ragdoll98@yandex.ru>
2023-10-08 22:53:08 +1000

    ProductiveBees = убрал рецепты ульев с мечом

--- .minecraft/kubejs/server_scripts/Productive Bees = убрал рецепты ульев с мечом.js
+++ .minecraft/kubejs/server_scripts/Productive Bees = убрал рецепты ульев с мечом.js
@@ -4,6 +4,16 @@ onEvent('recipes', e => {
 		input: Item.of('minecraft:iron_sword').ignoreNBT(),
 		type: 'minecraft:crafting_shapeless'
 	})
+	e.remove({
+		mod: 'productivebees',
+		input: Item.of('minecraft:wooden_sword').ignoreNBT(),
+		type: 'minecraft:crafting_shapeless'
+	})
+	e.remove({
+		mod: 'productivebees',
+		input: Item.of('minecraft:diamond_sword').ignoreNBT(),
+		type: 'minecraft:crafting_shapeless'
+	})
 	//Книжки пока бесполезны, отключил крафт
 	removeRecipeByID(e, [
 		'productivebees:guide_book',
