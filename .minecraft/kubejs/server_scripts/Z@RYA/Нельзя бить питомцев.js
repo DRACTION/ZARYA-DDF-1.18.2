a6d0d480ac2f326db1f7a5c2086af8ccc948a327
DRACTION <ragdoll98@yandex.ru>
2024-04-29 21:39:41 +1000

    Добавил в скрипт по блокировки урона по питомцам проверку на переменную OwnerUUID. Это позволяет не бить рекрутов (своих или друзей)

--- .minecraft/kubejs/server_scripts/Z@RYA/Нельзя бить питомцев.js
+++ .minecraft/kubejs/server_scripts/Z@RYA/Нельзя бить питомцев.js
@@ -3,8 +3,8 @@ onEvent('entity.hurt', e => {
     if (e.entity == null || e.source.getActual() == null) return
     if (e.source.getActual().isPlayer() && e.entity.isPlayer()) return
 
-    let targetHasOwner   = e.entity.fullNBT.contains('Owner')
-    let attackerHasOwner = e.source.getActual().fullNBT.contains('Owner')
+    let targetHasOwner   = e.entity.fullNBT.contains('Owner') || e.entity.fullNBT.contains('OwnerUUID')
+    let attackerHasOwner = e.source.getActual().fullNBT.contains('Owner') || e.source.getActual().fullNBT.contains('OwnerUUID')
 
     // e.server.tell(`e.entity.fullNBT.contains('Owner') = ${e.entity.fullNBT.contains('Owner')}`)
     // e.server.tell(`e.source.getActual().isPlayer() = ${e.source.getActual().isPlayer()}`)
