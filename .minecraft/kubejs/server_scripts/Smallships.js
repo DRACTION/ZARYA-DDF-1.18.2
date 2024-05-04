89e43e69791e8f38a53fe951033a89a319920599
DRACTION <ragdoll98@yandex.ru>
2024-05-05 00:09:09 +1000

    SmallShips = пушки и ядра можно сделать не посещая Ад + упрощена посадка лошади/мула на корабль.

--- .minecraft/kubejs/server_scripts/Smallships.js
+++ .minecraft/kubejs/server_scripts/Smallships.js
@@ -2,9 +2,23 @@ onEvent('recipes', e => {
 
 	// Пушечное ядро
 	e.remove({id: 'smallships:cannon_ball'})
+	e.shaped('smallships:cannon_ball',
+		[
+			'NNN',
+			'NPN',
+			'NNN'
+		],
+		{
+			N: '#forge:nuggets/iron',
+			P: 'minecraft:gunpowder'
+		}
+	)
 	e.recipes.createMixing('2x smallships:cannon_ball', [
-		'4x #forge:nuggets/brass',
-		'4x #forge:nuggets/iron',
+		'8x #forge:nuggets/iron',
+		'minecraft:gunpowder'
+	]).heated()
+	e.recipes.createMixing('2x smallships:cannon_ball', [
+		'#forge:ingots/iron',
 		'minecraft:gunpowder'
 	]).heated()
 
@@ -14,16 +28,27 @@ onEvent('recipes', e => {
 		'smallships:cannon',
 		[
 			'  R',
-			'IBI',
+			'III',
 			' SP'
 		],
 		{
 			R: 'supplementaries:rope',
-			B: 'create:brass_block',
 			I: 'minecraft:iron_block',
 			P: '#minecraft:planks',
 			S: '#minecraft:wooden_slabs'
 		}
 	)
+
+	// Парус и прочее, использующее поводок
+	e.replaceInput({mod: 'smallships'}, 'minecraft:lead', 'supplementaries:rope')
 	
+})
+
+onEvent("item.entity_interact", event => {
+	if (event.getPlayer().getRidingEntity() != null
+		&& event.getPlayer().getRidingEntity().getType().split(':')[0] == 'smallships'
+		&& (event.getTarget().getType() == 'minecraft:horse' || event.getTarget().getType() == 'minecraft:mule')) {
+		event.getTarget().startRiding(event.getPlayer().getRidingEntity(), true)
+		event.cancel()
+	}
 })
