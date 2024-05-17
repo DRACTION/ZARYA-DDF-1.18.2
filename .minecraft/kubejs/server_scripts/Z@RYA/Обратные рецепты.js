2f99677529ff757a3d2dea8eb67a083d22a6b595
DRACTION <ragdoll98@yandex.ru>
2024-05-18 01:53:30 +1000

    Дополнение рецептов из дерева

--- .minecraft/kubejs/server_scripts/Z@RYA/Обратные рецепты.js
+++ .minecraft/kubejs/server_scripts/Z@RYA/Обратные рецепты.js
@@ -1,65 +1,44 @@
 onEvent('recipes', e => {
 
-	var woods = [
-		'oak',
-		'dark_oak',
-		'spruce',
-		'birch',
-		'acacia',
-		'crimson',
-		'warped'
-	]
-	var likeCopperBlocks = [
-		'cut_copper',
-		'exposed_cut_copper',
-		'weathered_cut_copper',
-		'oxidized_cut_copper',
-		'waxed_cut_copper',
-		'waxed_exposed_cut_copper',
-		'waxed_weathered_cut_copper',
-		'waxed_oxidized_cut_copper',
-		'cobblestone',
-		'mossy_cobblestone',
-		'cut_red_sandstone',
-		'smooth_red_sandstone',
-		'prismarine',
-		'smooth_sandstone',
-		'smooth_quartz',
-		'granite',
-		'andesite',
-		'polished_andesite',
-		'diorite',
-		'polished_diorite',
-		'cobbled_deepslate',
-		'polished_deepslate',
-		'blackstone',
-		'polished_blackstone',
-		'smooth_stone'
-	]
-
-	woods.forEach(thing => {
+	allWoods.forEach(thing => {
 		//9 деревянных кнопок вместо одной
 		modifyShapelesID(
 			e, //Event
-			'minecraft:'+thing+'_button', //Result
-			'minecraft:'+thing+'_button', //Craft ID
+			`minecraft:${thing}_button`, //Result
+			`minecraft:${thing}_button`, //Craft ID
 			9, //Count
-			'minecraft:'+thing+'_planks' //Ingredients
+			`minecraft:${thing}_planks` //Ingredients
 		)
 		//из 9 деревянных кнопок = 1 доска
-		e.shaped('minecraft:'+thing+'_planks', [ 'XXX', 'XXX', 'XXX' ], { X: 'minecraft:'+thing+'_button' })
-		//из 2 деревянных полублоков = 1 цельный блок
-		// теперь есть мод Just vertical slabs
-		// e.shaped('minecraft:'+thing+'_planks', [ 'X', 'X' ], { X: 'minecraft:'+thing+'_slab' })
+		e.shaped(`minecraft:${thing}_planks`, [ 'XXX', 'XXX', 'XXX' ], { X: `minecraft:${thing}_button` })
+
+		//6 люков вместо 2
+		modifyShapedID(
+			e, //Event
+			`minecraft:${thing}_trapdoor`, //Result
+			`minecraft:${thing}_trapdoor`, //Craft ID
+			6, //Count
+			['XXX','XXX'], //pattern
+			{ X: `minecraft:${thing}_planks` } //Ingredients
+		)
+		//из 6 люков = 6 досок
+		e.shaped(`6x minecraft:${thing}_planks`, [ 'XXX', 'XXX' ], { X: `minecraft:${thing}_trapdoor` })
+
+		//6 степенек вместо 4
+		modifyShapedID(
+			e, //Event
+			`minecraft:${thing}_stairs`, //Result
+			`minecraft:${thing}_stairs`, //Craft ID
+			6, //Count
+			['X  ','XX ','XXX'], //pattern
+			{ X: `minecraft:${thing}_planks` } //Ingredients
+		)
+		//из 6 степенек = 6 досок
+		e.shaped(`6x minecraft:${thing}_planks`, ['X  ','XX ','XXX'], { X: `minecraft:${thing}_stairs` })
 	})
 
-	// теперь есть мод Just vertical slabs
-	// likeCopperBlocks.forEach(thing => {
-	// 	e.shaped('minecraft:'+thing, [ 'X', 'X' ], { X: 'minecraft:'+thing+'_slab' })
-	// })
-
 	colors.forEach(color => {
-		e.shapeless('3x minecraft:'+color+'_wool', 'minecraft:'+color+'_bed')
+		e.shapeless(`3x minecraft:${color}_wool`, `minecraft:${color}_bed`)
 	})
 
 	//Аметистовый блок = 4 аметистовых осколка
