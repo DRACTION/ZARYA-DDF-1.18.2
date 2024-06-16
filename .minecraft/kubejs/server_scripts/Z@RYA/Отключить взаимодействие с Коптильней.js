a87bf8d204d0eb78991ac8f127aa6ef41b844e0a
DRACTION <ragdoll98@yandex.ru>
2024-06-16 18:26:00 +1000

    Рефакторинг кода отключения взаимодействия с коптильней

--- .minecraft/kubejs/server_scripts/Z@RYA/Отключить взаимодействие с Коптильней.js
+++ .minecraft/kubejs/server_scripts/Z@RYA/Отключить взаимодействие с Коптильней.js
@@ -1,14 +1,25 @@
+let smokers = [
+  'minecraft:smoker',
+  'fantasyfurniture:nordic/oven',
+  'fantasyfurniture:venthyr/oven',
+  'fantasyfurniture:bone/skeleton/oven',
+  'fantasyfurniture:necrolord/oven',
+  'fantasyfurniture:royal/oven',
+  'fantasyfurniture:bone/wither/oven',
+  'fantasyfurniture:dunmer/oven'
+]
+let inputers = [
+  'minecraft:hopper',
+  'create:chute',
+  'create:andesite_funnel',
+  'create:brass_funnel',
+  'create:andesite_belt_funnel',
+  'create:brass_belt_funnel'
+]
+
 onEvent('block.right_click', e => {
 
-	if ((e.block.id == 'minecraft:smoker' 
-		|| e.block.id == 'fantasyfurniture:nordic/oven'
-		|| e.block.id == 'fantasyfurniture:venthyr/oven'
-		|| e.block.id == 'fantasyfurniture:bone/skeleton/oven'
-		|| e.block.id == 'fantasyfurniture:necrolord/oven'
-		|| e.block.id == 'fantasyfurniture:royal/oven'
-		|| e.block.id == 'fantasyfurniture:bone/wither/oven')
-		&& (!e.player.minecraftEntity.shiftKeyDown || e.getItem().isEmpty())) {
-
+	if (smokers.includes(e.block.id) && (!e.player.minecraftEntity.shiftKeyDown || e.getItem().isEmpty())) {
 		e.cancel()
 		e.player.tell(`§a[Farmer's Delight] Прямое взаимодействие с коптильней отключено, используйте костёр, сковороду и т.п.!`)
 	}
@@ -18,71 +29,33 @@ onEvent('block.right_click', e => {
 onEvent('block.place', e => {
 
 	let blockId = e.getBlock().id
-	let blockPos = e.getBlock().getPos()
 
 	// console.log(blockId);
-	if (blockId == 'minecraft:smoker'
-		|| blockId == 'fantasyfurniture:nordic/oven'
-		|| blockId == 'fantasyfurniture:venthyr/oven'
-		|| blockId == 'fantasyfurniture:bone/skeleton/oven'
-		|| blockId == 'fantasyfurniture:necrolord/oven'
-		|| blockId == 'fantasyfurniture:royal/oven'
-		|| blockId == 'fantasyfurniture:bone/wither/oven'
-		|| blockId == 'minecraft:hopper'
-		|| blockId == 'create:chute'
-		|| blockId == 'create:andesite_funnel'
-		|| blockId == 'create:brass_funnel'
-		|| blockId == 'create:andesite_belt_funnel'
-		|| blockId == 'create:brass_belt_funnel') {
-
-		let posiblePositions = [
-			blockPos.offset( 1, 0, 0),
-			blockPos.offset(-1, 0, 0),
-			blockPos.offset( 0, 1, 0),
-			blockPos.offset( 0,-1, 0),
-			blockPos.offset( 0, 0, 1),
-			blockPos.offset( 0, 0,-1)
-		];
-
-		if (blockId == 'minecraft:smoker'
-			|| blockId == 'fantasyfurniture:nordic/oven'
-			|| blockId == 'fantasyfurniture:venthyr/oven'
-			|| blockId == 'fantasyfurniture:bone/skeleton/oven'
-			|| blockId == 'fantasyfurniture:necrolord/oven'
-			|| blockId == 'fantasyfurniture:royal/oven'
-			|| blockId == 'fantasyfurniture:bone/wither/oven') {
-			posiblePositions.forEach(pos => {
-				let nearbyBlockId = e.getLevel().getBlock(pos).id;
-				if (nearbyBlockId == 'minecraft:hopper'
-					|| nearbyBlockId == 'create:chute'
-					|| nearbyBlockId == 'create:andesite_funnel'
-					|| nearbyBlockId == 'create:brass_funnel'
-					|| nearbyBlockId == 'create:andesite_belt_funnel'
-					|| nearbyBlockId == 'create:brass_belt_funnel') {
-					e.cancel();
-					return;
-				}
-			})
-		} else if (blockId == 'minecraft:hopper'
-			|| blockId == 'create:chute'
-			|| blockId == 'create:andesite_funnel'
-			|| blockId == 'create:brass_funnel'
-			|| blockId == 'create:andesite_belt_funnel'
-			|| blockId == 'create:brass_belt_funnel') {
-
-			posiblePositions.forEach(pos => {
-				let nearbyBlockId = e.getLevel().getBlock(pos).id;
-				if (nearbyBlockId == 'minecraft:smoker'
-					|| nearbyBlockId == 'fantasyfurniture:nordic/oven'
-					|| nearbyBlockId == 'fantasyfurniture:venthyr/oven'
-					|| nearbyBlockId == 'fantasyfurniture:bone/skeleton/oven'
-					|| nearbyBlockId == 'fantasyfurniture:necrolord/oven'
-					|| nearbyBlockId == 'fantasyfurniture:royal/oven'
-					|| nearbyBlockId == 'fantasyfurniture:bone/wither/oven') {
-					e.cancel();
-					return;
-				}
-			})
-		}
+	let itsSmoker
+	if (smokers.includes(blockId)) {
+		itsSmoker = true
+	} else if (inputers.includes(blockId)) {
+		itsSmoker = false
+	} else {
+		return
 	}
+
+	let blockPos = e.getBlock().getPos()
+	let posiblePositions = [
+		blockPos.offset( 1, 0, 0),
+		blockPos.offset(-1, 0, 0),
+		blockPos.offset( 0, 1, 0),
+		blockPos.offset( 0,-1, 0),
+		blockPos.offset( 0, 0, 1),
+		blockPos.offset( 0, 0,-1)
+	];
+
+	posiblePositions.forEach(pos => {
+		let nearbyBlockId = e.getLevel().getBlock(pos).id
+		if((itsSmoker && inputers.includes(nearbyBlockId))
+			|| (!itsSmoker && smokers.includes(nearbyBlockId))) {
+			e.cancel()
+			return
+		}
+	})
 })
