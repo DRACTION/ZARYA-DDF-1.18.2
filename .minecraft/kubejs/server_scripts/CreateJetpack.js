89f752e880c880aa461f3254f8cf6fb682e288fb
DRACTION <ragdoll98@yandex.ru>
2024-06-16 21:05:12 +1000

    CreateJetpack = исправление и рефакторинг рецепта

--- .minecraft/kubejs/server_scripts/CreateJetpack.js
+++ .minecraft/kubejs/server_scripts/CreateJetpack.js
@@ -1,45 +1,30 @@
 onEvent('recipes', e => {
 
-  e.remove({id:'create_jetpack:jetpack'})
-  e.recipes.create.mechanical_crafting('create_jetpack:jetpack', [
-    '   PSP   ',
-    '  PESEP  ',
-    ' PMESEMP ',
-    'PCMEBEMCP',
-    'PLPGIGPLP',
-    'PCTP PTCP'
-  ], {
-    P: '#forge:plates/brass',
-    S: 'create:shaft',
-    M: 'create:precision_mechanism',
-    E: 'createaddition:electrum_spool',
-    C: 'create:smart_chute',
-    B: 'create:copper_backtank',
-    L: 'create:propeller',
-    G: 'create:sequenced_gearshift',
-    I: 'create:linked_controller',
-    T: 'create:electron_tube'
-  }).id('zarya:create_jetpack')
+	let mat = [
+		'   PSP   ',
+		'  PESEP  ',
+		' PMESEMP ',
+		'PCMEBEMCP',
+		'PLPGIGPLP',
+		'PCTP PTCP'
+	]
+	let ingr = {
+		P: '#forge:plates/brass',
+		S: 'create:shaft',
+		M: 'create:precision_mechanism',
+		E: 'createaddition:gold_spool',
+		C: 'create:smart_chute',
+		B: 'create:copper_backtank',
+		L: 'create:propeller',
+		G: 'create:sequenced_gearshift',
+		I: 'create:linked_controller',
+		T: 'create:electron_tube'
+	}
+	e.remove({id:'create_jetpack:jetpack'})
+	e.recipes.create.mechanical_crafting('create_jetpack:jetpack', mat, ingr).id('zarya:create_jetpack')
 
-  e.remove({id:'create_jetpack:netherite_jetpack'})
-  e.recipes.create.mechanical_crafting('create_jetpack:netherite_jetpack', [
-    '   PSP   ',
-    '  PESEP  ',
-    ' PMESEMP ',
-    'PCMEBEMCP',
-    'PLPGIGPLP',
-    'PCTP PTCP'
-  ], {
-    P: '#forge:plates/brass',
-    S: 'create:shaft',
-    M: 'create:precision_mechanism',
-    E: 'createaddition:electrum_spool',
-    C: 'create:smart_chute',
-    B: 'create:netherite_backtank',
-    L: 'create:propeller',
-    G: 'create:sequenced_gearshift',
-    I: 'create:linked_controller',
-    T: 'create:electron_tube'
-  }).id('zarya:create_netherite_jetpack')
+	ingr.B = 'create:netherite_backtank'
+	e.remove({id:'create_jetpack:netherite_jetpack'})
+	e.recipes.create.mechanical_crafting('create_jetpack:netherite_jetpack', mat, ingr).id('zarya:create_netherite_jetpack')
 
 })
