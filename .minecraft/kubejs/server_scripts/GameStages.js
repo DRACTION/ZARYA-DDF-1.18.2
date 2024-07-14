c437d645b3835c8fe5605cf501e4ab857a18e3d9
DRACTION <ragdoll98@yandex.ru>
2024-07-15 00:25:17 +1000

    InControl + GameStages = Теперь нёрф монстров срабатывает сразу при появлении нового игрока, без костылей. Также полная шкала насыщенности для новых игроков.

--- .minecraft/kubejs/server_scripts/GameStages.js
+++ .minecraft/kubejs/server_scripts/GameStages.js
@@ -1,13 +1,16 @@
+onEvent('player.logged_in', e => {
+    if (e.player.stages.getAll().isEmpty()) {
+        e.player.stages.add('nerf_enemies')
+        e.getServer().runCommandSilent(`heal ${e.getPlayer().toString()}`)
+    }
+})
+
 onEvent('player.advancement', e => {
     // e.server.tell(`e.getAdvancement().getId() = ${e.getAdvancement().getId()}`)
     // e.server.tell(`e.player.stages.has('enter_the_nether') = ${e.player.stages.has('enter_the_nether')}`)
     // e.server.tell(`e.player.stages.has('enter_the_end') = ${e.player.stages.has('enter_the_end')}`)
 
-    if (e.getAdvancement().getId() == 'create:root' && !e.player.stages.has('nerf_enemies')) {
-
-        e.player.stages.add('nerf_enemies')
-
-    } else if (e.getAdvancement().getId() == 'minecraft:story/enter_the_nether' && !e.player.stages.has('enter_the_nether')) {
+    if (e.getAdvancement().getId() == 'minecraft:story/enter_the_nether' && !e.player.stages.has('enter_the_nether')) {
 
         e.player.stages.add('enter_the_nether')
         if (e.player.stages.has('nerf_enemies')) {
