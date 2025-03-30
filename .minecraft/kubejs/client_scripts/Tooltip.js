caf954248eb34d50a5a97927d868f4a934556d6c
DRACTION <ragdoll98@yandex.ru>
2025-03-30 18:43:20 +1000

    Waystone = изменение крафтов свитков + подписи к свиткам.

--- .minecraft/kubejs/client_scripts/Tooltip.js
+++ .minecraft/kubejs/client_scripts/Tooltip.js
@@ -16,5 +16,9 @@ onEvent('item.tooltip', e => {
   e.add(['kubejs:firestarter'], Text.of('Используется для розжига').darkGray())
 
   e.add(['pettracker:tracker'], Text.of('ПКМ на мобе, чтобы добавить в книгу').darkGray())
+
+  e.add(['waystones:return_scroll'], Text.of('Телепортирует к ближайшему обелиску').green())
+  e.add(['waystones:bound_scroll'], Text.of('Телепортирует к ранее привязанному обелиску (ПКМ по обелиску)').green())
+  e.add(['waystones:warp_scroll'], Text.of('Телепортирует к любому обелиску на выбор').green())
   
 })
--- .minecraft/kubejs/server_scripts/Waystone.js
+++ .minecraft/kubejs/server_scripts/Waystone.js
@@ -1,21 +1,47 @@
 onEvent('recipes', e => {
-  // Усложнение крафта
-  modifyShapedID(
-    e, //Event
-    'waystones:warp_stone', //Result
-    'waystones:warp_stone', //Craft ID
-    1, //Count
-    [
-      'EFE',
-      'PMP',
-      'PDP'
-    ], //Pattern
-    {
-      M:'create:precision_mechanism',
-      F:'minecraft:purple_dye',
-      P:'minecraft:ender_pearl',
-      D:'minecraft:diamond',
-      E:'minecraft:emerald'
-    } //Ingredients
-  )
+	//////////// Свиток возврата ////////////
+	modifyShapedID(e, 'waystones:return_scroll', 'waystones:return_scroll', 3, [
+		'WIW',
+		'PPP',
+		'WGW'], {
+		W:'waystones:warp_dust',
+		P:'minecraft:paper',
+		I:'minecraft:ink_sac',
+		G:'minecraft:gold_nugget'
+	})
+	//////////// Свиток привязки ////////////
+	modifyShapedID(e, 'waystones:bound_scroll', 'waystones:bound_scroll', 1, [
+		' E ',
+		'DSD'], {
+		S:Item.of('waystones:return_scroll').ignoreNBT(),
+		D:'apotheosis:gem_dust',
+		E:'minecraft:ender_pearl'
+	})
+	//////////// Свиток перемещения ////////////
+	modifyShapedID(e, 'waystones:warp_scroll', 'waystones:warp_scroll', 1, [
+		' I ',
+		'MSM'], {
+		S:Item.of('waystones:bound_scroll').ignoreNBT(),
+		M:'apotheosis:rare_material',
+		I:'minecraft:glow_ink_sac'
+	})
+	//////////// Камень перемещения ////////////
+	modifyShapedID(
+		e, //Event
+		'waystones:warp_stone', //Result
+		'waystones:warp_stone', //Craft ID
+		1, //Count
+		[
+			'EFE',
+			'PMP',
+			'PDP'
+		], //Pattern
+		{
+			M:'create:precision_mechanism',
+			F:'minecraft:purple_dye',
+			P:'minecraft:ender_pearl',
+			D:'minecraft:diamond',
+			E:'minecraft:emerald'
+		} //Ingredients
+	)
 })
