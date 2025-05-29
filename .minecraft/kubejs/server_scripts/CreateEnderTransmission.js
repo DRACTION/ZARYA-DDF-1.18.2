f95890e09dbf78fbf98a97feb2e8cef1d99f850c
DRACTION <ragdoll98@yandex.ru>
2025-05-29 21:37:54 +1000

    Усложнение крафта передатчиков.

--- .minecraft/kubejs/server_scripts/CreateEnderTransmission.js
+++ .minecraft/kubejs/server_scripts/CreateEnderTransmission.js
@@ -1,26 +1,27 @@
 onEvent('recipes', e => {
 	let mat = [
 		'OOXOO',
-		'OPWPO',
-		'OPEPO',
-		'OPWPO',
+		'OMWMO',
+		'OEPEO',
+		'OMWMO',
 		'OOXOO'
 	]
 	let ingr = {
 		O: 'minecraft:obsidian',
 		E: 'minecraft:ender_eye',
-		P: 'minecraft:ender_pearl',
+		P: 'bosses_of_mass_destruction:void_thorn',
+		M: 'apotheosis:epic_material',
 		W: Item.of('waystones:warp_stone').ignoreNBT(),
 		X: 'create:shaft'
 	}
-
+	//////////////// Передатчик энергии ////////////////
 	e.remove({id:'createendertransmission:energy_transmitter'})
 	e.recipes.create.mechanical_crafting('2x createendertransmission:energy_transmitter', mat, ingr)
-
+	//////////////// Передатчик предметов ////////////////
 	ingr.X = 'create:chute'
 	e.remove({id:'createendertransmission:item_transmitter'})
 	e.recipes.create.mechanical_crafting('2x createendertransmission:item_transmitter', mat, ingr)
-
+	//////////////// Передатчик жидкости ////////////////
 	ingr.X = 'create:mechanical_pump'
 	e.remove({id:'createendertransmission:fluid_transmitter'})
 	e.recipes.create.mechanical_crafting('2x createendertransmission:fluid_transmitter', mat, ingr)
