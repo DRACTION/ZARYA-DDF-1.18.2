429bd289451221357cf676a5742666341cc4a49c
DRACTION <ragdoll98@yandex.ru>
2025-06-14 13:57:23 +1000

    Кастеты = перевод и поменял местами улучшение

--- .minecraft/kubejs/server_scripts/EF_Knuckles.js
+++ .minecraft/kubejs/server_scripts/EF_Knuckles.js
@@ -1,11 +1,13 @@
 onEvent('recipes', e => {
-
-  e.remove({id:'ef_weapon_extended:ironknucklesrecipe'})
-  e.smithing('ef_weapon_extended:iron_knuckles', Item.of('epicfight:glove').ignoreNBT(), '#forge:plates/iron')
+  ///////////////// Золотой кастет /////////////////
   e.remove({id:'ef_weapon_extended:goldknucklesrecipe'})
-  e.smithing('ef_weapon_extended:gold_knuckles', Item.of('ef_weapon_extended:iron_knuckles').ignoreNBT(), '#forge:plates/gold')
+  e.smithing('ef_weapon_extended:gold_knuckles', Item.of('epicfight:glove').ignoreNBT(), '#forge:plates/gold')
+  ///////////////// Железный кастет /////////////////
+  e.remove({id:'ef_weapon_extended:ironknucklesrecipe'})
+  e.smithing('ef_weapon_extended:iron_knuckles', Item.of('ef_weapon_extended:gold_knuckles').ignoreNBT(), '#forge:plates/iron')
+  ///////////////// Алмазный кастет /////////////////
   e.remove({id:'ef_weapon_extended:diamondknucklesrecipe'})
   e.smithing('ef_weapon_extended:diamond_knuckles', Item.of('ef_weapon_extended:gold_knuckles').ignoreNBT(), '#forge:plates/diamond')
+  ///////////////// Незеритовый кастет /////////////////
   e.replaceInput({id: 'ef_weapon_extended:netheriteknucklesrecipe'}, 'minecraft:netherite_ingot', '#forge:plates/netherite')
-
 })
