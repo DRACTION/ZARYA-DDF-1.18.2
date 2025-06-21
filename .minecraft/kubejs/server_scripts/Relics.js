4d82c06503e12a5903d9162823488520017f5563
DRACTION <ragdoll98@yandex.ru>
2025-06-21 17:34:52 +1000

    Relics = столу исследователя добавлен тег ломания топором

--- .minecraft/kubejs/server_scripts/Relics.js
+++ .minecraft/kubejs/server_scripts/Relics.js
@@ -7,3 +7,7 @@ onEvent('lootjs', event => {
     .randomChance(0.08)
     .addLoot('relics:relic_experience_bottle')
 })
+
+onEvent('block.tags', e => {
+  e.add('minecraft:mineable/axe', 'relics:researching_table')
+})
