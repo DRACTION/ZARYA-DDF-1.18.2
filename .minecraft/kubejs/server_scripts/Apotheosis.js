fc94a5a171aa54f96c8573a426f1d69d4bdb159f
DRACTION <ragdoll98@yandex.ru>
2025-07-04 23:08:00 +1000

    Apotheosis = упрощение крафта стола перековки

--- .minecraft/kubejs/server_scripts/Apotheosis.js
+++ .minecraft/kubejs/server_scripts/Apotheosis.js
@@ -1,5 +1,5 @@
 onEvent('recipes', event => {
-	
+	//////////////////// Апгрейд материалов ////////////////////
 	let mat = [
 		"XXX",
 		"XOX",
@@ -9,5 +9,17 @@ onEvent('recipes', event => {
 	event.shaped('2x apotheosis:rare_material', mat, { X: 'apotheosis:uncommon_material', O: 'apotheosis:rare_material' })
 	event.shaped('2x apotheosis:epic_material', mat, { X: 'apotheosis:rare_material', O: 'apotheosis:epic_material' })
 	event.shaped('2x apotheosis:mythic_material', mat, { X: 'apotheosis:epic_material', O: 'apotheosis:mythic_material' })
-	
+	//////////////////// Стол перековки ////////////////////
+	event.remove({id:'apotheosis:reforging_table'})
+	event.shaped('apotheosis:reforging_table', [
+		'SIS',
+		'DED',
+		'SNS'
+	], {
+		S: 'minecraft:netherite_scrap',
+		I: 'irons_spellbooks:arcane_ingot',
+		D: 'apotheosis:gem_dust',
+		E: 'minecraft:enchanting_table',
+		N: 'minecraft:nether_bricks'
+	})
 })
