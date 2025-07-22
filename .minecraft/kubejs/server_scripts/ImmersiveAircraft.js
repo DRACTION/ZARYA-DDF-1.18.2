efb93f618f159ce00bde93e8c7d37bf591b397ae
DRACTION <ragdoll98@yandex.ru>
2025-07-22 21:22:16 +1000

    ImmersiveAircraft = альтернативный рецепт Бройлера (попроще)

--- .minecraft/kubejs/server_scripts/ImmersiveAircraft.js
+++ .minecraft/kubejs/server_scripts/ImmersiveAircraft.js
@@ -1,3 +1,14 @@
 onEvent('recipes', event => {
+	///////////// Бойлер /////////////
+	event.shaped('immersive_aircraft:boiler', [
+		'S',
+		'F',
+		'G'
+	], {
+		S: 'create:steam_engine',
+		F: 'create:fluid_tank',
+		G: 'createlowheated:basic_burner'
+	})
+	///////////// Квадрокоптер /////////////
 	event.replaceInput({id: 'immersive_aircraft:quadrocopter'}, 'minecraft:string', 'farmersdelight:safety_net')
 })
