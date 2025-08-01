2390b75b89eb5826b90c9cd6f68352c33f8025c5
DRACTION <ragdoll98@yandex.ru>
2025-08-01 22:32:32 +1000

    Фикс, что нельзя было бить прислужников боссов

--- .minecraft/kubejs/server_scripts/Z@RYA/Cant hit pets.js
+++ .minecraft/kubejs/server_scripts/Z@RYA/Cant hit pets.js
@@ -3,15 +3,24 @@ onEvent('entity.hurt', e => {
     if (e.entity == null || e.source.getActual() == null) return
     if (e.source.getActual().isPlayer() && e.entity.isPlayer()) return
 
+    //e.server.tell(`e.entity.fullNBT.contains('Summoner') = ${e.entity.fullNBT.contains('Summoner')}`)
+    //e.server.tell(`e.entity.fullNBT.getUUID('Summoner') = ${e.entity.fullNBT.getUUID('Summoner')}`)
+    //e.server.tell(`e.getLevel().minecraftLevel.getPlayerByUUID(e.entity.fullNBT.getUUID('Summoner')) = ${e.getLevel().minecraftLevel.getPlayerByUUID(e.entity.fullNBT.getUUID('Summoner'))}`)
+    //e.server.tell(`e.getLevel().minecraftLevel.getPlayerByUUID(e.entity.fullNBT.getUUID('Summoner')) != null = ${e.getLevel().minecraftLevel.getPlayerByUUID(e.entity.fullNBT.getUUID('Summoner')) != null}`)
+    //e.server.tell(`e.getLevel().minecraftLevel.getPlayerByUUID(e.entity.fullNBT.getUUID('Summoner')).isPlayer() = ${e.getLevel().minecraftLevel.getPlayerByUUID(e.entity.fullNBT.getUUID('Summoner')).isPlayer()}`)
+    //e.server.tell(`Utils.server.playerList.getPlayer(e.entity.fullNBT.getUUID('Summoner')) = ${Utils.server.playerList.getPlayer(e.entity.fullNBT.getUUID('Summoner'))}`)
+
     let targetHasOwner   = e.entity.getType() == 'tombstone:spectral_wolf'
-                        || e.entity.fullNBT.contains('Owner')
-                        || e.entity.fullNBT.contains('OwnerUUID')
-                        || e.entity.fullNBT.contains('Summoner')
+                        || (e.entity.fullNBT.contains('Owner')     && e.getLevel().minecraftLevel.getPlayerByUUID(e.entity.fullNBT.getUUID('Owner')) != null)
+                        || (e.entity.fullNBT.contains('OwnerUUID') && e.getLevel().minecraftLevel.getPlayerByUUID(e.entity.fullNBT.getUUID('OwnerUUID')) != null)
+                        || (e.entity.fullNBT.contains('Summoner')  && e.getLevel().minecraftLevel.getPlayerByUUID(e.entity.fullNBT.getUUID('Summoner')) != null)
 
     let attackerHasOwner = e.source.getActual().getType() == 'tombstone:spectral_wolf'
-                        || e.source.getActual().fullNBT.contains('Owner')
-                        || e.source.getActual().fullNBT.contains('OwnerUUID')
-                        || e.source.getActual().fullNBT.contains('Summoner')
+                        || (e.source.getActual().fullNBT.contains('Owner')     && e.getLevel().minecraftLevel.getPlayerByUUID(e.source.getActual().fullNBT.getUUID('Owner')) != null)
+                        || (e.source.getActual().fullNBT.contains('OwnerUUID') && e.getLevel().minecraftLevel.getPlayerByUUID(e.source.getActual().fullNBT.getUUID('OwnerUUID')) != null)
+                        || (e.source.getActual().fullNBT.contains('Summoner')  && e.getLevel().minecraftLevel.getPlayerByUUID(e.source.getActual().fullNBT.getUUID('Summoner')) != null)
+
+    // e.source.getActual().fullNBT.contains('Summoner') && Utils.server.playerList.getPlayer(e.entity.fullNBT.Summoner)
 
     // e.server.tell(`e.entity.fullNBT.contains('Owner') = ${e.entity.fullNBT.contains('Owner')}`)
     // e.server.tell(`e.source.getActual().isPlayer() = ${e.source.getActual().isPlayer()}`)
