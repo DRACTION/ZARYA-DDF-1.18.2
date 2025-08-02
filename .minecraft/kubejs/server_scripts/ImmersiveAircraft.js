ffc473049b223dd6fa660669fc0b1231a26d8c6e
DRACTION <ragdoll98@yandex.ru>
2025-08-02 14:33:34 +1000

    ImmersiveAircraft = самолёт может сломать кто угодно - возможность посадить или снять спутника с дирижабля.

--- .minecraft/kubejs/server_scripts/ImmersiveAircraft.js
+++ .minecraft/kubejs/server_scripts/ImmersiveAircraft.js
@@ -12,3 +12,26 @@ onEvent('recipes', event => {
 	///////////// Квадрокоптер /////////////
 	event.replaceInput({id: 'immersive_aircraft:quadrocopter'}, 'minecraft:string', 'farmersdelight:safety_net')
 })
+
+onEvent("item.entity_interact", event => {
+
+	if (passengers.includes(event.getTarget().getType()) || ownerIsPlayer(event, event.getTarget())) {
+
+		if (event.getPlayer().getRidingEntity() != null
+			&& (event.getPlayer().getRidingEntity().getType() == 'immersive_aircraft:gyrodyne'
+			|| event.getPlayer().getRidingEntity().getType() == 'immersive_aircraft:airship'
+			|| event.getPlayer().getRidingEntity().getType() == 'immersive_aircraft:cargo_airship')) {
+
+			event.getTarget().startRiding(event.getPlayer().getRidingEntity(), false)
+			event.cancel()
+
+		} else if (event.getTarget().getRidingEntity() != null
+			&& (event.getTarget().getRidingEntity().getType() == 'immersive_aircraft:gyrodyne'
+			|| event.getTarget().getRidingEntity().getType() == 'immersive_aircraft:airship'
+			|| event.getTarget().getRidingEntity().getType() == 'immersive_aircraft:cargo_airship')) {
+
+			event.getTarget().dismountRidingEntity()
+			event.cancel()
+		}
+	}
+})
