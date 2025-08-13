f885e2bd9745b8265ebbf007841131dcbaceecfa
DRACTION <ragdoll98@yandex.ru>
2025-08-13 22:15:01 +1000

    EnchantingInfuser = усложнил крафт, too OP, так как late-game

--- .minecraft/kubejs/server_scripts/EnchantingInfuser.js
+++ .minecraft/kubejs/server_scripts/EnchantingInfuser.js
@@ -1,31 +1,40 @@
 onEvent('recipes', e => {
 	//////////// Нагнетатель чар ////////////
-	modifyShapedID(e, 'enchantinginfuser:enchanting_infuser', 'enchantinginfuser:enchanting_infuser', 1, [
-		'CPC',
-		'AMA',
-		'CTC'], {
-		C: 'minecraft:crying_obsidian',
-		P: 'create_enchantment_industry:printer',
-		A: 'minecraft:amethyst_shard',
+	e.remove({ id: "enchantinginfuser:enchanting_infuser" })
+	e.recipes.create.mechanical_crafting('enchantinginfuser:enchanting_infuser', [
+		' OGO ',
+		'OAFAO',
+		'CMLMC',
+		'OASAO',
+		' ORO '], {
+		A: 'irons_spellbooks:arcane_rune',
+		O: 'minecraft:crying_obsidian',
+		R: 'apotheosis:rare_material',
+		C: 'minecraft:amethyst_cluster',
 		M: 'create:precision_mechanism',
-		T: 'minecraft:enchanting_table'
+		F: 'apotheosis:lucky_foot',
+		L: 'apotheosis:library',
+		S: 'apotheosis:sightshelf',
+		G: 'irons_spellbooks:magic_cloth'
 	})
 	//////////// Улучшенный нагнетатель чар ////////////
 	e.remove({ id: "enchantinginfuser:advanced_enchanting_infuser" })
 	e.recipes.create.mechanical_crafting('enchantinginfuser:advanced_enchanting_infuser', [
-		' OGO ',
-		'ONHNO',
+		' EGE ',
+		'ONLNO',
 		'ORHRO',
 		'OMDMO',
-		' OIO '], {
+		' AIA '], {
+		A: 'irons_spellbooks:arcane_salvage',
 		O: 'minecraft:crying_obsidian',
 		I: 'enchantinginfuser:enchanting_infuser',
 		N: 'minecraft:netherite_ingot',
 		M: 'create:precision_mechanism',
-		H: 'create_enchantment_industry:hyper_experience_bottle',
+		H: 'irons_spellbooks:cooldown_upgrade_orb',
 		D: 'create_enchantment_industry:disenchanter',
 		R: 'create_enchantment_industry:experience_rotor',
+		E: 'apotheosis:epic_material',
+		L: 'apotheosis:ender_library',
 		G: Item.of('create_enchantment_industry:enchanting_guide').ignoreNBT()
 	})
-	
 })
